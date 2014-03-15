c1777208f530ef1d037962be2156c3e3698dee4b
messense <wapdevelop@gmail.com>
2014-03-15 20:02:23 +0800

    init

---
+++ theme.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+name = 'seven'
+author = 'Teambition'
+homepage = 'https://github.com/messense/catsup-theme-seven'
+post_per_page = 3
+vars = {
+    "links": [
+        {
+            "name": "catsup",
+            "url": "https://github.com/whtsky/catsup",
+            "description": "Awesome!"
+        }
+    ]
+}
